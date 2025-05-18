9d5032c59ba7ed08e953c43a2f5b92ea1a128355
Leul Yiheyis <leulykpro@gmail.com>
2025-05-18 10:41:05 +0300

    order properties by review ratings

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,7 +6,8 @@ INNER JOIN users ON bookings.user_id = users.user_id;
 -- Query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 
 SELECT * FROM properties
-LEFT JOIN reviews ON reviews.property_id = properties.property_id;
+LEFT JOIN reviews ON reviews.property_id = properties.property_id
+ORDER BY rating;
 
 -- Query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 -- FULL OUTER JOIN is not supported in MySQL, so opted to use left and right joins with union.
